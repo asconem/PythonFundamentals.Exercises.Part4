0f199b4dbfcd16e0453bfd95046407535de581a9
asconem <asconem@gmail.com>
2020-05-08 16:13:37 -0400

    Linear fibonacci script complete.

---
+++ fibonacci_linear.py
@@ -0,0 +1,21 @@
+user_number = int(input("Pick a number zero or greater: "))
+while user_number < 0:
+    user_number = int(input("NO NEGATIVES! Pick again: "))
+
+
+def fibonacci(num):
+    a = 0
+    b = 1
+    if num == 0:
+        return a
+    elif num == 1:
+        return b
+    else:
+        for i in range(2, num):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+
+print(fibonacci(user_number))
