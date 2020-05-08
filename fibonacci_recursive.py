02e002eb444b20fa5d0ef1f056348cf3771746d1
asconem <asconem@gmail.com>
2020-05-08 15:45:53 -0400

    Renamed function

--- fibonacci_recursive.py
+++ fibonacci_recursive.py
@@ -3,11 +3,11 @@ while user_number < 1 or user_number > 30:
     user_number = int(input("Invalid selection. Pick a number between 1 and 30: "))
 
 
-def fib_sequence(num):
+def fibonacci(num):
     if num <= 1:
         return num
     else:
-        return fib_sequence(num - 1) + fib_sequence(num - 2)
+        return fibonacci(num - 1) + fibonacci(num - 2)
 
 
-print(fib_sequence(user_number))
+print(fibonacci(user_number))
